b5766cb5b6ebeea9948a137e545d27d443035e45
Edem Akato <augustine.akato@amalitech.com>
2024-11-07 10:51:18 +0000

    feat: error catching and cleanup

--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -3,54 +3,63 @@ import java.util.Scanner;
 
 public class MatrixMultiplication {
 
-        //initialise the matrix size
-        public int rowA, rowB, colA, colB;
+                                                                          //initialise the matrix size
+    public int rowA, rowB, colA, colB;
 
-        //initialise the matrices using
-        public int [][] matrixA, matrixB;
+                                                                        //initialise the matrices using
+    public int [][] matrixA, matrixB;
 
-        public void collect (){
+    public void collect (){
 
-        //collect the size of both matrices from the user
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number of rows of the Matrix A: ");
-        rowA = scanner.nextInt();
+        try(Scanner scanner = new Scanner((System.in))) {
 
-        System.out.print("Number of columns of the Matrix A: ");
-        colA = scanner.nextInt();
+                                                                        //collect the size of both matrices from the user
+            System.out.print("Number of rows of the Matrix A: ");
+            rowA = scanner.nextInt();
 
-        System.out.print("Number of rows of the Matrix B: ");
-        rowB = scanner.nextInt();
+            System.out.print("Number of columns of the Matrix A: ");
+            colA = scanner.nextInt();
 
-        System.out.print("Number of columns of the Matrix B: ");
-        colB = scanner.nextInt();
+            System.out.print("Number of rows of the Matrix B: ");
+            rowB = scanner.nextInt();
 
-        System.out.print("enter the numbers for Matrix A: ");
+            System.out.print("Number of columns of the Matrix B: ");
+            colB = scanner.nextInt();
 
-        matrixA = new int[rowA][colA];
-        matrixB = new int[rowB][colB];
+            System.out.println("enter the numbers for Matrix A");
+            System.out.println();
 
-        //check if the matrices have compatible sizes
-        if(colA != rowB) {
-            System.out.println("incompatible matrices");
-            return;
-        }
+            matrixA = new int[rowA][colA];
+            matrixB = new int[rowB][colB];
 
-        //collect the array elements for both arrays
-        for (int i = 0; i < rowA; i++){
-            for (int j = 0; j<colA; j++){
-                System.out.println("Enter value for Matrix A" + "["  + i + "]" + "[" + j + "]");
-                matrixA[i][j] = scanner.nextInt();
+                                                                    //check if the matrices have compatible sizes
+            if (colA != rowB) {
+                System.out.println("incompatible matrices");
+                return;
             }
-        }
-        for (int i = 0; i < rowB; i++){
-            for (int j = 0; j<colB; j++){
-                System.out.println("Enter value for Matrix B" + "["  + i + 1 + "]" + "[" + (j+ 1) + "]");
-                matrixB[i][j] = scanner.nextInt();
+
+
+                                                                     //collect the array elements for both arrays
+            for (int i = 0; i < rowA; i++) {
+                for (int j = 0; j < colA; j++) {
+                    System.out.print("Enter value for Matrix A" + "[" + i + "]" + "[" + j + "] :");
+                    matrixA[i][j] = scanner.nextInt();
+                }
             }
+
+            System.out.println();
+
+            for (int i = 0; i < rowB; i++) {
+                for (int j = 0; j < colB; j++) {
+                    System.out.print("Enter value for Matrix B" + "[" + i + 1 + "]" + "[" + (j + 1) + "] :");
+                    matrixB[i][j] = scanner.nextInt();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("something went wrong");
         }
 
-        //print out matrices for the user
+                                                                //print out matrices for the user
         System.out.println("Matrix A");
         System.out.println(Arrays.deepToString(matrixA));
 
@@ -61,25 +70,25 @@ public class MatrixMultiplication {
 
     }
 
-        public void multiply (){
-//    public int [][] multiply (int [] [] matrixA, int [][] matrixB, int colA, int colB, int rowA, int rowB){
+    public void multiply (){
 
 
 
-        //initialise the resulting matrix using the rowA and colB
-        int [][] resultingMatrix = new int[rowA][colB];
+                                                                //initialise the resulting matrix using the rowA and colB
+        int [][] matrixC = new int[rowA][colB];
 
-        //compute the elements of the resulting matrix by looping through matrixA and matrixB
+                                                                //compute the elements of the resulting matrix by looping through matrixA and matrixB
         for (int i = 0; i < rowA ;i++ ) {
             for (int j = 0; j < colB; j++) {
                 for (int x = 0; x < colA; x++) {
-                    resultingMatrix[i][j] += matrixA[i][x] * matrixB[x][j];
+                    matrixC[i][j] += matrixA[i][x] * matrixB[x][j];
                 }
             }
         }
 
-        //print out the resulting matrix with preceding and trailing |
-        for(int [] array : resultingMatrix){
+                                                                //print out the resulting matrix with preceding and trailing |
+        System.out.println("Matrix C");
+        for(int [] array : matrixC){
             System.out.print("| ");
             for (int i : array){
                 System.out.print(i + " ");
@@ -93,4 +102,5 @@ public class MatrixMultiplication {
         matrixMultiplication.collect();
         matrixMultiplication.multiply();
     }
+
 }
